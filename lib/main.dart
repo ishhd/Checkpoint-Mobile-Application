26954e8145847ba8b7222185f3a168e0db15b8b0
Shahad <Shahadabdualah@gmail.com>
2021-03-29 11:45:20 +0300

    edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tester/Screens/AcademicStaff/CourseAS.dart';
-import 'package:tester/Screens/AcademicStaff/homePageAS.dart';
-import 'package:tester/Screens/Administrator/EFAdmin.dart';
-import 'package:tester/Screens/Administrator/homepage_administrator.dart';
-import 'package:tester/Screens/Student/homePageStudent.dart';
-import 'Screens/ResetPass.dart';
 import 'Screens/SignIn.dart';
-
-import 'package:tester/Screens/Administrator/CourseInfo.dart';
-import 'package:tester/Screens/Administrator/homepage_administrator.dart';
-
 import 'package:tester/Screens/SignIn.dart';
-
 import 'package:tester/Screens/model/student.dart';
 import 'package:tester/Screens/services/auth.dart';
-import 'Screens/signUp.dart';
-import 'Screens/Student/homePageStudent.dart';
 
 void main() {
   runApp(MyApp());
